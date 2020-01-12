f54418b1daac37231e8fccdd9ca373b2a1433492
Douglas Varollo <dcvarollo@gmail.com>
2020-01-12 13:56:59 -0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // iniciando o DB
-mongoose.connect('mongodb://127.0.0.1:27017/nodeapi', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
 });
 
